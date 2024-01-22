2f2fd429a4412f3461f516cce570cb6345685d0d
Vitor Klein <vitorhklein2@gmail.com>
2024-01-22 00:06:18 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-20 w-full bg-dark">
+        <h1 className="text-primary">Hello World!</h1>
+      </div>
+
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,20 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'primary': '#FFD600',
+        'secondary': '#E8D3A4',
+        'terciary': '#BFAC87',
+        'primary white': '#F9E19C',
+        'texts': '#FFF7E0',
+        'dark': '#1B1A1F',
+      }
     },
   },
   plugins: [],
